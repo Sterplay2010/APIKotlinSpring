ef37c5e5969e85c1aa84ebc4793ec2ae76ba4972
sterplay2010 <sterplay2010@gmail.com>
2021-07-19 00:22:06 -0500

    API ACTUALIZADA

---
+++ src/main/kotlin/com/sterplay2010/api/mongodb/kotlinmongoapi/WebConfiguration.kt
@@ -0,0 +1,12 @@
+package com.sterplay2010.api.mongodb.kotlinmongoapi
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfiguration : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**").allowedMethods("*")
+    }
+}
