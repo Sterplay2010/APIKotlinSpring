0c1aed9abcc452070f964e341e91f980e674a2ef
sterplay2010 <sterplay2010@gmail.com>
2021-07-19 00:01:17 -0500

    API MODIFICADA

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: java -jar -Dserver.port=$PORT  gogster/gogster1.0.jar
+web: java -jar -Dserver.port=$PORT  gogster/gogster.jar
--- src/main/kotlin/com/sterplay2010/api/mongodb/kotlinmongoapi/GameController.kt
+++ src/main/kotlin/com/sterplay2010/api/mongodb/kotlinmongoapi/GameController.kt
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/game")
-@CrossOrigin(origins = ["*"],maxAge = 3600,allowCredentials = "true")
 class GameController(private val gameRepository: GameRepository) {
 
     @GetMapping
